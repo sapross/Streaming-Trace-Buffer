b5f8fd697c97e18d6f1d6fa0cf49d6a0292d2ac3
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2022-11-11 17:06:52 +0100

    Rewrote Tracebuffer to better address issues.
    Cursory testing makes tracebuffer appear fully functional.

--- src/FPGATraceBuffer.vhd
+++ src/FPGATraceBuffer.vhd
@@ -39,33 +39,68 @@ entity FPGA_TRACE_BUFFER is
     -- CLK from FPGA fabric.
     FPGA_CLK_I          : in    std_logic;
     -- Trigger/Enable signal.
-    FPGA_TE_I           : in    std_logic;
+    FPGA_TRIG_I         : in    std_logic;
     -- Trace input
     FPGA_TRACE_I        : in    std_logic;
     -- Bit serial output for data transfer.
-    FPGA_DOUT_O         : out   std_logic
+    FPGA_DOUT_O         : out   std_logic;
+    -- FPGA side signal indicating fulfilled trigger condition and trace acquisition.
+    FPGA_TRIG_O         : out   std_logic
   );
 end entity FPGA_TRACE_BUFFER;
 
 architecture BEHAVIORAL of FPGA_TRACE_BUFFER is
 
-  signal addr_low                     : integer range 0 to TRB_WIDTH - 1;
-  signal addr_high, addr_high_prev    : integer range 0 to TRB_DEPTH - 1;
+  type state_t is (st_idle, st_wait_for_trigger, st_capture_trace, st_rwmode, st_done);
 
-  signal timer                        : integer range 0 to TRB_BITS - 1;
+  signal state,     state_next                       : state_t;
 
-  signal status                       : status_t;
-  signal config                       : config_t;
+  signal addr_low,  addr_low_next                    : integer range 0 to TRB_WIDTH - 1;
+  signal addr_high                                   : integer range 0 to TRB_DEPTH - 1;
+  signal addr_high_next                              : integer range 0 to TRB_DEPTH - 1;
+  signal addr_high_prev                              : integer range 0 to TRB_DEPTH - 1;
 
-  signal sticky_trigger               : std_logic;
-  signal pointer_inc                  : std_logic;
+  signal timer,     timer_next                       : integer range 0 to TRB_BITS - 1;
 
-  signal trace_reg                    : std_logic_vector(TRB_WIDTH - 1 downto 0);
+  signal status,    status_next                      : status_t;
+  signal config                                      : config_t;
+
+  signal trace_reg, trace_reg_next                   : std_logic_vector(TRB_WIDTH - 1 downto 0);
+  signal fpga_serial_valid                           : std_logic;
+
+  function get_limit (
+    constant config_i : config_t
+  ) return integer is
+  begin
+
+    -- Formular for the limit L:
+    -- Let n := timer_stop, P := TRB_BITS, N := 2**timer_stop'length
+    -- L = (n+1)/N * P - 1
+    -- Example: Trace Buffer can hold 64 bits (P = 64), timer_stop is 3
+    -- bits (N = 8 => 0<n<8) and timer_stop is set to "011" (n=3):
+    -- L = (3 + 1)/ 8 * 64 - 1 = 31
+    return ((to_integer(unsigned(config.timer_stop)) + 1) * TRB_BITS) /
+      (2 ** config.timer_stop'length) - 1;
+
+  end function;
 
 begin
 
   READ_ADDR_O  <= std_logic_vector(to_unsigned(addr_high, TRB_ADDR_BITS));
   WRITE_ADDR_O <= std_logic_vector(to_unsigned(addr_high_prev, TRB_ADDR_BITS));
+  FPGA_DOUT_O  <= trace_reg(addr_low);
+
+  -- Change function of FPGA_TRIG_O based on config.
+  FPGA_TRIGGER_OUTPUT : process (status, config, fpga_serial_valid) is
+  begin
+
+    if (config.TE_mode = "0") then
+      FPGA_TRIG_O <= status.tr_hit(0);
+    else
+      FPGA_TRIG_O <= not status.tr_hit(0) and fpga_serial_valid;
+    end if;
+
+  end process FPGA_TRIGGER_OUTPUT;
 
   -- Synchronous update of mode signal and status output.
   CONFIG_STATUS_UPDATE : process (FPGA_CLK_I) is
@@ -74,115 +109,153 @@ begin
     if rising_edge(FPGA_CLK_I) then
       config <= slv_to_config(CONFIG_I);
       -- Reset is held for exactly one cycle.
-      if (config.reset = "1") then
-        config.reset <= "0";
-      end if;
       STATUS_O <= status_to_slv(status);
     end if;
 
   end process CONFIG_STATUS_UPDATE;
 
-  -- Denpending on te_mode, TE_I is either sticky or treated simply as an
-  -- enable.
-  TRIGGER : process (FPGA_CLK_I) is
+  FSM_CORE : process (FPGA_CLK_I) is
   begin
 
     if rising_edge(FPGA_CLK_I) then
       if (config.reset = "1") then
-        sticky_trigger    <= '0';
-        status.event_pos  <= (others => '0');
-        status.event_addr <= (others => '0');
-      else
-        if (config.te_mode = "0") then
-          if (FPGA_TE_I = '1' and  sticky_trigger = '0') then
-            status.event_pos  <= std_logic_vector(to_unsigned(addr_low, TRB_WIDTH_BITS));
-            status.event_addr <= std_logic_vector(to_unsigned(addr_high_prev, TRB_ADDR_BITS));
-            sticky_trigger    <= '1';
-          end if;
-        else
-          sticky_trigger <= FPGA_TE_I;
-          if (FPGA_TE_I = '1') then
-            status.event_pos  <= std_logic_vector(to_unsigned(addr_low, TRB_WIDTH_BITS));
-            status.event_addr <= std_logic_vector(to_unsigned(addr_high, TRB_ADDR_BITS));
-          end if;
-        end if;
-      end if;
-    end if;
-
-  end process TRIGGER;
-
-  -- Starts a timer if trigger is set and disables pointer increment on timer overflow.
-  --
-  TIMER_PROC : process (FPGA_CLK_I) is
-  begin
-
-    if rising_edge(FPGA_CLK_I) then
-      if (config.reset = "1") then
-        timer         <= 0;
-        pointer_inc   <= '1';
-        status.tr_hit <= "0";
-      else
-        if (FPGA_TE_I = '1' or sticky_trigger = '1') then
-          -- Formular for the limit L:
-          -- Let n := timer_stop, P := TRB_BITS, N := 2**timer_stop'length
-          -- L = (n+1)/N * P
-          -- Example: Trace Buffer can hold 64 bits (P = 64), timer_stop is 3
-          -- bits (N = 8 => 0<n<8) and timer_stop is set to "011" (n=3):
-          -- L = (3 + 1)/ 8 * 64 = 32
-          if (timer < ((to_integer(unsigned(config.timer_stop)) + 1) * TRB_BITS) / ((2 ** config.timer_stop'length)) - 1) then
-            timer <= timer + 1;
-          else
-            pointer_inc   <= '0';
-            status.tr_hit <= "1";
-          end if;
-        end if;
-      end if;
-    end if;
-
-  end process TIMER_PROC;
-
-  ADDR_PROC : process (FPGA_CLK_I) is
-  begin
-
-    if rising_edge(FPGA_CLK_I) then
-      if (config.reset = "1") then
-        addr_low <= 0;
-        -- Since the write happens one cycle after addr_high is incremented,
-        -- addr_high needs to be initialized with ones to start filling the
-        -- bram at address zero.
+        state          <= st_idle;
+        status         <= STATUS_DEFAULT;
+        addr_low       <= 0;
         addr_high      <= 0;
         addr_high_prev <= TRB_DEPTH - 1;
-        WE_O           <= '0';
-        DATA_O         <= (others => '0');
+        timer          <= 0;
         trace_reg      <= (others => '0');
-        FPGA_DOUT_O    <= '0';
+
+        WE_O   <= '0';
+        DATA_O <= (others => '0');
       else
-        if (pointer_inc = '1') then
-          trace_reg(addr_low) <= FPGA_TRACE_I;
-          FPGA_DOUT_O         <= trace_reg(addr_low);
+        if (config.enable = "1") then
+          state          <= state_next;
+          status         <= status_next;
+          addr_low       <= addr_low_next;
+          addr_high      <= addr_high_next;
+          addr_high_prev <= addr_high;
+          timer          <= timer_next;
+          trace_reg      <= trace_reg_next;
 
-          if (addr_low < TRB_WIDTH - 1) then
-            addr_low <= addr_low + 1;
-            WE_O     <= '0';
-          else
-            addr_low <= 0;
-            -- Upon overflow of the lower address exchange trace_reg with the
-            -- word of the trace buffer.
-            WE_O      <= '1';
-            DATA_O    <= FPGA_TRACE_I & trace_reg(TRB_WIDTH - 2 downto 0);
-            trace_reg <= DATA_I;
+          fpga_serial_valid <= '0';
 
-            addr_high_prev <= addr_high;
-            if (addr_high < TRB_DEPTH - 1) then
-              addr_high <= addr_high + 1;
-            else
-              addr_high <= 0;
+          WE_O   <= '0';
+          DATA_O <= (others => '0');
+
+          if (state = st_wait_for_trigger or state = st_capture_trace) then
+            -- Output current bit of the trace register.
+            if (addr_low = TRB_WIDTH - 1) then
+              -- trace_register is full, write out to buffer.
+              WE_O      <= '1';
+              DATA_O    <= FPGA_TRACE_I & trace_reg(TRB_WIDTH - 2 downto 0);
+              trace_reg <= DATA_I;
+            end if;
+          end if;
+          if (state = st_rwmode) then
+            fpga_serial_valid <= '1';
+            if (addr_low = 0) then
+              trace_reg <= DATA_I;
+            end if;
+            -- Output current bit of the trace register.
+            if (FPGA_TRIG_I = '1' and addr_low = TRB_WIDTH - 1) then
+              -- trace_register is full, write out to buffer.
+              WE_O   <= '1';
+              DATA_O <= FPGA_TRACE_I & trace_reg(TRB_WIDTH - 2 downto 0);
             end if;
           end if;
         end if;
       end if;
     end if;
 
-  end process ADDR_PROC;
+  end process FSM_CORE;
+
+  FSM : process (state, addr_low, addr_high, timer, trace_reg, config, FPGA_TRACE_I, FPGA_TRIG_I) is
+  begin
+
+    if (config.reset = "1") then
+      state_next <= st_idle;
+    else
+      state_next     <= state;
+      status_next    <= status;
+      addr_low_next  <= addr_low;
+      addr_high_next <= addr_high;
+      timer_next     <= timer;
+      trace_reg_next <= trace_reg;
+
+      case state is
+
+        when st_idle =>
+          -- Reset address high and low;
+          addr_low_next  <= 0;
+          addr_high_next <= 0;
+          -- TE_mode switches between TraceBuffer- and RWBuffer-mode.
+          if (config.te_mode = "0") then
+            state_next <= st_wait_for_trigger;
+          else
+            state_next <= st_rwmode;
+          end if;
+
+        when st_wait_for_trigger =>
+          -- Keep incrementing trace buffer address.
+          addr_low_next <= (addr_low + 1) mod TRB_WIDTH;
+          if (addr_low_next = TRB_WIDTH - 1) then
+            addr_high_next <= (addr_high + 1) mod TRB_DEPTH;
+          end if;
+          -- Populate trace register with data.
+          trace_reg_next(addr_low) <= FPGA_TRACE_I;
+
+          if (FPGA_TRIG_I = '1') then
+            -- Trigger has been activated.
+            state_next <= st_capture_trace;
+            -- Save position of trigger event in trace buffer.
+            status_next.event_pos  <= std_logic_vector(to_unsigned(addr_low_next, TRB_WIDTH_BITS));
+            status_next.event_addr <= std_logic_vector(to_unsigned(addr_high,   TRB_ADDR_BITS));
+          end if;
+
+        when st_capture_trace =>
+          -- Keep incrementing trace buffer address.
+          addr_low_next <= (addr_low + 1) mod TRB_WIDTH;
+          if (addr_low_next = TRB_WIDTH - 1) then
+            addr_high_next <= (addr_high + 1) mod TRB_DEPTH;
+          end if;
+          -- Populate trace register with data.
+          trace_reg_next(addr_low) <= FPGA_TRACE_I;
+
+          if (timer < get_limit(config)) then
+            timer_next <= timer + 1;
+          else
+            state_next         <= st_done;
+            status_next.tr_hit <= "1";
+          end if;
+
+        when st_rwmode =>
+          if (FPGA_TRIG_I = '1') then
+            if (timer < get_limit(config)) then
+              timer_next <= timer + 1;
+            else
+              state_next         <= st_done;
+              status_next.tr_hit <= "1";
+            end if;
+
+            trace_reg_next(addr_low) <= FPGA_TRACE_I;
+
+            addr_low_next <= (addr_low + 1) mod TRB_WIDTH;
+            if (addr_low = TRB_WIDTH - 1) then
+              addr_high_next <= (addr_high + 1) mod TRB_DEPTH;
+            end if;
+            status_next.event_pos  <= std_logic_vector(to_unsigned(addr_low_next, TRB_WIDTH_BITS));
+            status_next.event_addr <= std_logic_vector(to_unsigned(addr_high,   TRB_ADDR_BITS));
+          end if;
+
+        when st_done =>
+          status_next.tr_hit <= "1";
+
+      end case;
+
+    end if;
+
+  end process FSM;
 
 end architecture BEHAVIORAL;
--- tb/TB_FPGATraceBuffer.vhd
+++ tb/TB_FPGATraceBuffer.vhd
@@ -22,32 +22,33 @@ end entity TB_FPGATRACEBUFFER;
 
 architecture TB of TB_FPGATRACEBUFFER is
 
-  constant CLK_PERIOD              : time    := 1 ns;           -- ns;
+  constant CLK_PERIOD                  : time    := 1 ns;           -- ns;
 
-  signal config_slv                : std_logic_vector(CONFIG_BITS - 1 downto 0);
-  signal config                    : config_t;
+  signal config_slv                    : std_logic_vector(CONFIG_BITS - 1 downto 0);
+  signal config                        : config_t;
 
-  signal status_slv                : std_logic_vector(STATUS_BITS - 1 downto  0);
-  signal status : status_t;
-  signal read_addr                 : std_logic_vector(TRB_ADDR_BITS - 1 downto 0);
-  signal write_addr                : std_logic_vector(TRB_ADDR_BITS - 1 downto 0);
-  signal we                        : std_logic;
-  signal clk                       : std_logic;
+  signal status_slv                    : std_logic_vector(STATUS_BITS - 1 downto  0);
+  signal status                        : status_t;
+  signal read_addr                     : std_logic_vector(TRB_ADDR_BITS - 1 downto 0);
+  signal write_addr                    : std_logic_vector(TRB_ADDR_BITS - 1 downto 0);
+  signal we                            : std_logic;
+  signal clk                           : std_logic;
 
-  signal data_in, data_out         : std_logic_vector(TRB_WIDTH - 1 downto 0);
+  signal data_in, data_out             : std_logic_vector(TRB_WIDTH - 1 downto 0);
 
-  signal te                        : std_logic;
-  signal trace                     : std_logic;
-  signal dout                      : std_logic;
+  signal trig_i                        : std_logic;
+  signal trig_o                        : std_logic;
+  signal trace                         : std_logic;
+  signal dout                          : std_logic;
 
   type ram_t is array(0 to TRB_DEPTH - 1) of std_logic_vector(TRB_WIDTH - 1 downto 0);
 
-  signal ram                       : ram_t := (others => (others => '0'));
+  signal ram                           : ram_t := (others => (others => '0'));
 
 begin
 
   config_slv <= config_to_slv(config);
-  status <= slv_to_status(status_slv);
+  status     <= slv_to_status(status_slv);
 
   FPGA_TRACE_BUFFER_1 : entity work.fpga_trace_buffer
     port map (
@@ -59,9 +60,10 @@ begin
       DATA_O       => data_out,
       DATA_I       => data_in,
       FPGA_CLK_I   => clk,
-      FPGA_TE_I    => te,
+      FPGA_TRIG_I    => trig_i,
       FPGA_TRACE_I => trace,
-      FPGA_DOUT_O  => dout
+      FPGA_DOUT_O  => dout,
+      FPGA_TRIG_O  => trig_o
     );
 
   CLK_PROCESS : process is
@@ -86,21 +88,26 @@ begin
 
   end process RAM_PROC;
 
-  RAND_TRACE : process (clk) is
+  RAND_TRACE : process is
 
     variable seed1, seed2 : positive := 1;
     variable x            : real;
 
   begin
 
-    if rising_edge(clk) then
+    while true loop
+
       uniform(seed1, seed2, x);
-      if x < 0.5 then
+
+      if (x < 0.5) then
         trace <= '0';
       else
         trace <= '1';
       end if;
-    end if;
+
+      wait for CLK_PERIOD;
+
+    end loop;
 
   end process RAND_TRACE;
 
@@ -111,27 +118,65 @@ begin
     wait for 1 ps;
     config        <= CONFIG_DEFAULT;
     config.reset  <= "1";
-    te            <= '0';
+    trig_i        <= '0';
     wait for CLK_PERIOD;
     config.reset  <= "0";
     wait for CLK_PERIOD;
     config.enable <= "1";
+
     wait for 20 * CLK_PERIOD;
-    te            <= '1';
+    report "Testing Trigger functionality with default timer stop (full future)";
+    trig_i <= '1';
     wait for CLK_PERIOD;
-    te            <= '0';
+    trig_i <= '0';
+
     while status.tr_hit = "0" loop
+
       wait for CLK_PERIOD;
+
     end loop;
-    config.reset  <= "1";
+
+    report "Testing Trigger functionality with centered timer stop (centered event)";
+    config.reset      <= "1";
     config.timer_stop <= "011";
     wait for CLK_PERIOD;
-    config.reset  <= "0";
+    config.reset      <= "0";
+    wait for 2 * CLK_PERIOD;
+    trig_i            <= '1';
     wait for CLK_PERIOD;
-    te <= '1';
-    wait for CLK_PERIOD;
-    te <= '0';
+    trig_i            <= '0';
 
+    report "Testing Trigger functionality with immediate timer stop (full history)";
+    config.reset      <= "1";
+    config.timer_stop <= "000";
+    wait for CLK_PERIOD;
+    config.reset      <= "0";
+    wait for 2 * CLK_PERIOD;
+    trig_i            <= '1';
+    wait for CLK_PERIOD;
+    trig_i            <= '0';
+
+    report "Testing Data Transfer functionality";
+    config.reset      <= "1";
+    config.te_mode    <= "1";
+    config.timer_stop <= "111";
+    config.enable     <= "0";
+    wait for CLK_PERIOD;
+    config.reset      <= "0";
+    wait for CLK_PERIOD;
+    config.enable     <= "1";
+
+    while (trig_o = '0') loop
+
+      wait for CLK_PERIOD;
+
+    end loop;
+
+    trig_i <= '1';
+    wait for CLK_PERIOD;
+    trig_i <= '0';
+    wait for 5 * CLK_PERIOD;
+    trig_i <= '1';
     wait;
 
   end process MAIN;
