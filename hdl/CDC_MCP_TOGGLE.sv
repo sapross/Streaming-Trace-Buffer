7fb40262a17468bfe4d6b211b3fa0b74f07d3c96
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2023-01-01 17:51:37 +0100

    Better port naming scheme.

--- hdl/CDC_MCP_TOGGLE.sv
+++ hdl/CDC_MCP_TOGGLE.sv
@@ -15,46 +15,48 @@ module CDC_MCP_TOGGLE #(
                         parameter integer unsigned WIDTH = 5
                         ) (
                            // Signals from clock domain A.
-                           input logic              CLKA_I,
+                           input logic              CLK_A_I,
                            // Data to be synchronized.
-                           input logic [WIDTH-1:0]  DA_I,
+                           input logic [WIDTH-1:0]  DATA_A_I,
                            // Store signal for synchronization.
-                           input logic              STA_I,
+                           input logic              SYNC_A_I,
 
                            // Signals from clock domain B.
-                           input logic              CLKB_I,
-                           output logic [WIDTH-1:0] DB_O
+                           input logic              CLK_B_I,
+                           output logic [WIDTH-1:0] DATA_B_O,
+                           output logic             SYNC_B_O
                            ) ;
 
-   // Register input DA_I in reg_a
+   // Register input DATA_A_I in reg_a
    logic [WIDTH-1:0]                                reg_a;
-   always_ff @(posedge CLKA_I) begin
-      if (STA_I) begin
-         reg_a = DA_I;
+   always_ff @(posedge CLK_A_I) begin
+      if (SYNC_A_I) begin
+         reg_a = DATA_A_I;
       end
    end
 
-   //Turn pulse in STA_I into toggle signal.
+   //Turn pulse in SYNC_A_I into toggle signal.
    logic                                            toggle_st;
-   always_ff @(posedge CLKA_I) begin : PULSE_GEN
-      toggle_st = STA_I ^ toggle_st;
+   always_ff @(posedge CLK_A_I) begin : PULSE_GEN
+      toggle_st = SYNC_A_I ^ toggle_st;
    end
 
    // Using toggle_st, produce a synchronized pulse.
    logic [2:0] q_b;
-   always_ff @(posedge CLKB_I) begin : PULSE_SYNCHRONIZER
+   always_ff @(posedge CLK_B_I) begin : PULSE_SYNCHRONIZER
       q_b = {q_b[2:1],toggle_st};
    end
    logic       ld_b;
    assign ld_b = q_b[2] ^ q_b[1];
 
+   assign SYNC_B_O = ld_b;
    // Register reg_a in clock domain B using synchronized pulse.
    logic [WIDTH-1:0]                                reg_b;
-   always_ff @(posedge CLKB_I) begin
+   always_ff @(posedge CLK_B_I) begin
       if (ld_b) begin
          reg_b = reg_a;
       end
    end
-   assign DB_O = reg_b;
+   assign DATA_B_O = reg_b;
 
 endmodule // CDC_MCP_TOGGLE
